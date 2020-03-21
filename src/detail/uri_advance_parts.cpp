b6737d828fbc02581b356e404234c3c3fa6ccf9d
Vasily Lobaskin <liastre@outlook.com>
2020-03-21 20:54:34 +0300

    Fix recursion on some compilers due to wrong copy_part method used

--- src/detail/uri_advance_parts.cpp
+++ src/detail/uri_advance_parts.cpp
@@ -8,29 +8,32 @@
 #include <utility>
 #include <limits>
 
-namespace network {
-namespace detail {
+namespace network_detail = network::detail;
+using network::detail::uri_part;
+using network::string_view;
+
 namespace {
 template <class Iterator>
 uri_part copy_part(Iterator first, Iterator last,
                    string_view::const_iterator &it) {
   auto part_first = it;
   std::advance(it, std::distance(first, last));
-  return detail::uri_part(part_first, it);
+  return network_detail::uri_part(part_first, it);
 }
 }  // namespace
 
-uri_part copy_part(const std::string &uri, string_view::const_iterator &it) {
-  return copy_part(std::begin(uri), std::end(uri), it);
+uri_part network_detail::copy_part(const std::string &uri,
+                                   string_view::const_iterator &it) {
+  return ::copy_part(std::begin(uri), std::end(uri), it);
 }
 
-void advance_parts(string_view uri_view, uri_parts &parts,
-                   const uri_parts &existing_parts) {
+void network_detail::advance_parts(string_view uri_view, uri_parts &parts,
+                                   const uri_parts &existing_parts) {
   auto first = std::begin(uri_view);
 
   auto it = first;
   if (auto scheme = existing_parts.scheme) {
-    parts.scheme = copy_part(std::begin(*scheme), std::end(*scheme), it);
+    parts.scheme = ::copy_part(std::begin(*scheme), std::end(*scheme), it);
 
     // ignore : for all URIs
     if (*it == ':') {
@@ -45,32 +48,31 @@ void advance_parts(string_view uri_view, uri_parts &parts,
 
   if (auto user_info = existing_parts.hier_part.user_info) {
     parts.hier_part.user_info =
-      copy_part(std::begin(*user_info), std::end(*user_info), it);
+        ::copy_part(std::begin(*user_info), std::end(*user_info), it);
     ++it;  // ignore @
   }
 
   if (auto host = existing_parts.hier_part.host) {
-    parts.hier_part.host = copy_part(std::begin(*host), std::end(*host), it);
+    parts.hier_part.host = ::copy_part(std::begin(*host), std::end(*host), it);
   }
 
   if (auto port = existing_parts.hier_part.port) {
     ++it;  // ignore :
-    parts.hier_part.port = copy_part(std::begin(*port), std::end(*port), it);
+    parts.hier_part.port = ::copy_part(std::begin(*port), std::end(*port), it);
   }
 
   if (auto path = existing_parts.hier_part.path) {
-    parts.hier_part.path = copy_part(std::begin(*path), std::end(*path), it);
+    parts.hier_part.path = ::copy_part(std::begin(*path), std::end(*path), it);
   }
 
   if (auto query = existing_parts.query) {
     ++it;  // ignore ?
-    parts.query = copy_part(std::begin(*query), std::end(*query), it);
+    parts.query = ::copy_part(std::begin(*query), std::end(*query), it);
   }
 
   if (auto fragment = existing_parts.fragment) {
     ++it;  // ignore #
-    parts.fragment = copy_part(std::begin(*fragment), std::end(*fragment), it);
+    parts.fragment =
+        ::copy_part(std::begin(*fragment), std::end(*fragment), it);
   }
 }
-}  // namespace detail
-}  // namespace network
